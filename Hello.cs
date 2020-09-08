fb7ab5ece6e799dde691e4d2914f2e4cf2f45780
fuyun <scfido@gmail.com>
2020-09-08 18:53:51 +0800

    WebAssembly 项目框架

---
+++ Hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Video
+{
+
+
+    public class Player
+    {
+        static string Play()
+        {
+            return "ok";
+        }
+    }
+}
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir .output
+
+export WASM_SDK="/Users/fuyun/Downloads/mono-wasm"
+# for eg:
+# $WASM_SDK="D:\WasmSDK\"
+# DONT FORGET THE TRAILING SLASH
+
+csc /target:library \
+   /out:.output/player.dll \
+   /noconfig /nostdlib \
+   /r:$WASM_SDK/wasm-bcl/wasm/mscorlib.dll \
+   /r:$WASM_SDK/wasm-bcl/wasm/System.dll \
+   /r:$WASM_SDK/wasm-bcl/wasm/System.Core.dll \
+   /r:$WASM_SDK/wasm-bcl/wasm/Facades/netstandard.dll \
+   /r:$WASM_SDK/wasm-bcl/wasm/System.Net.Http.dll \
+   /r:$WASM_SDK/framework/WebAssembly.Bindings.dll \
+    Hello.cs
+
+#   /r:$WASM_SDK/framework/WebAssembly.Net.Http.dll \
+mono $WASM_SDK/packager.exe --copy=always --out=./wwwroot .output/player.dll
